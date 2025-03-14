2ae6e61fed4ca376b7fd7985a3b9184dc616c4b8
HIBA BEGHDI <beghiba@gmail.com>
2025-03-14 10:42:14 +0000

    OUT-90 the admin can manage the interests (visualize, add, update & delete)

--- observer-client/app/(services)/interestApi.tsx
+++ observer-client/app/(services)/interestApi.tsx
@@ -23,6 +23,36 @@ const interestService = {
       return [];
     }
   },
+
+
+  createInterest: async (data: { category: string; description: string }): Promise<Interest> => {
+    try {
+      const response = await axiosInstance.post('/interests', data);
+      return response.data;
+    } catch (error) {
+      console.error('Error creating interest:', error);
+      throw error;
+    }
+  },
+
+  updateInterest: async (id: string, data: { category: string; description: string }): Promise<Interest> => {
+    try {
+      const response = await axiosInstance.patch(`/interests/${id}`, data);
+      return response.data;
+    } catch (error) {
+      console.error('Error updating interest:', error);
+      throw error;
+    }
+  },
+
+  deleteInterest: async (id: string): Promise<void> => {
+    try {
+      await axiosInstance.delete(`/interests/${id}`);
+    } catch (error) {
+      console.error('Error deleting interest:', error);
+      throw error;
+    }
+  },
 };
 
 export default interestService;
